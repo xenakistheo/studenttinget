f2b336c9667832520fac453512ab9f43a349aade
xenakistheo <theoxen03@gmail.com>
2025-02-09 11:14:09 +0100

    cleanup: remove old Schema

--- models.py
+++ models.py
@@ -6,25 +6,6 @@ class User(BaseModel):
     email: str
     password: str
 
-class CompanySchema(BaseModel):
-    company_name: Optional[str]
-    location: Optional[str]
-    started: Optional[int]
-    organization_number: Optional[int]
-    type: Optional[str]
-    productservicesolution: Optional[str]
-    employees: Optional[int]  
-    revenue_2023: Optional[int]
-    sector: Optional[str]
-    origin: Optional[str]
-    collaboration: Optional[str]
-    nfr_program: Optional[str]
-    project_title: Optional[str]
-    period: Optional[str]
-    amount_mnok: Optional[float]  
-    nfr_projects: Optional[List[Dict[str, Union[str, float]]]]
-
-
 class VedtakSchema(BaseModel):
     saksnummer: Optional[str]
     aarstall: Optional[int]
